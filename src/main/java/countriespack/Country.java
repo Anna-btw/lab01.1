0e2f2e4f5d2c53e6f0204894f83d05cb7d4457e0
Anna Ivkina <ak.ivkina@severmed.com>
2025-02-03 22:51:59 +0300

    правки: Нейминга атрибутов, Модификаторов доступа, Конструкторов и сеттеров, Массива данных

--- src/main/java/countriespack/Country.java
+++ src/main/java/countriespack/Country.java
@@ -1,94 +1,120 @@
 package countriespack;
 
 public class Country {
-    String nameCountry;
-    int areaCountry;
-    int populationCountry;
-    String nameCapital;
-    Integer areaCapital;    //используем Integer для возможности null
-    int populationDensity;
+    private String name; // private чтобы атрибутами класса можно оперировать только внутри класса
+    private int area;
+    private int population;
+    private String capital;
+    private Integer areaCapital;    //используем Integer для возможности null
+    private int densityPopulation;
 
-    // Конструктор класса, принимающий значения всех атрибутов
-    public Country(String nameCountry, int areaCountry, int populationCountry, String nameCapital, Integer areaCapital) {
-        this.nameCountry = nameCountry;
-        this.areaCountry = areaCountry;
-        this.populationCountry = populationCountry;
-        this.nameCapital = nameCapital;
+    // Конструктор класса, принимающий значения всех атрибутов, через this
+    /*
+    public Country(String name, int area, int population, String capital, Integer areaCapital) {
+        this.name = name;
+        this.area = area;
+        this.population = population;
+        this.capital = capital;
         this.areaCapital = areaCapital;
+     */
+    // Конструктор класса, принимающий значения всех атрибутов, через setter
+    public Country (String name, int area, int population, String capital, Integer areaCapital) {
+        setName(name);
+        setArea(area);
+        setPopulation(population);
+        setCapital(capital);
+        setAreaCapital(areaCapital);
+        // ф-ция вычисления, вынесли логику вычисления из конструктора
+        calculateDensityPopulation(population,area);
 
     // В конструкторе класса проверяем перед делением, что areaCountry != 0
-        if (areaCountry != 0) {
-            this.populationDensity = populationCountry / areaCountry;
-        } else {
-            this.populationDensity = 0;
-            System.out.println("Площадь страны не может быть нулевой. Плотность населения установлена = 0");
-        }
+
     }
 
     // Конструктор для городов-государств
-    public Country(String nameCountry, int areaCountry, int populationCountry) {
-        this.nameCountry = nameCountry;
-        this.areaCountry = areaCountry;
-        this.populationCountry = populationCountry;
-        this.nameCapital = null;
-        this.areaCapital = null;
-        // вычисляем плотность населения чрз тернарный оператор
-        // (тут_условие) ? тут_выражение_ктр_выполняется_при_условии_true : тут_выражение_ктр_выполняется_при_условии_false
-        this.populationDensity = (areaCountry != 0) ? populationCountry / areaCountry : 0;
+    public Country(String name, int area, int population) {
+        setName(name);
+        setArea(area);
+        setPopulation(population);
+        capital = null; // можно не писать this. , так как в Конструктор не передаем capital и areaCapital
+        areaCapital = null;
+        calculateTernDensityPopulation(population, area);
     }
 
     //get-методы, возвращающие значения атрибутов класса
-    public String getNameCountry() {
-        return nameCountry;
+    public String getName() {
+        return name;
     }
 
-    public int getAreaCountry() {
-        return areaCountry;
+    public int getArea() {
+        return area;
     }
 
-    public int getPopulationCountry() {
-        return populationCountry;
+    public int getPopulation() {
+        return population;
     }
 
-    public String getNameCapital() {
-        return nameCapital;
+    public String getCapital() {
+        return capital;
     }
 
     public Integer getAreaCapital() {
         return areaCapital;
     }
 
-    public int getPopulationDensity() {
-        return populationDensity;
+    public int getDensityPopulation() {
+        return densityPopulation;
+    }
+
+    //в ф-ции высчитываем и устанавливаем densityPopulation
+    private void calculateDensityPopulation(int population, int area) {
+        if (area == 0) {
+            this.densityPopulation = 0;
+            System.out.println("Площадь страны не может быть нулевой. Плотность населения установлена = 0");
+        }
+        this.densityPopulation = population / area;
+    }
+
+    //в ф-ции чрз тернарный оператор высчитываем плотность населения
+    private void calculateTernDensityPopulation(int population, int area) {
+        // (тут_условие) ? тут_выражение_ктр_выполняется_при_условии_true : тут_выражение_ктр_выполняется_при_условии_false
+        this.densityPopulation = (area != 0) ? population / area : 0;
     }
 
     //set-методы, устанавливающие значения атрибутов, в т.ч.: площадь и население >0, название !null
-    public void setNameCountry(String nameCountry) {
-        if (nameCountry != null && !nameCountry.isEmpty()) {
-            this.nameCountry = nameCountry;
-        } else {
-            throw new IllegalArgumentException("Ошибка. Название не должно быть пустой ссылкой");
-        }
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Ошибка. Название не должно быть пустой ссылкой");}
+        this.name = name;
     }
 
-    public void setAreaCountry(int areaCountry) {
-        if (areaCountry > 0){
-            this.areaCountry = areaCountry;
+    //1й ВАРИАНТ условия, через позитивную проверку
+    /*
+    public void setArea(int area) {
+        if (area > 0){
+            this.area = area;
     } else {
             throw new IllegalArgumentException("Ошибка. Площадь должна быть >0");
         }
     }
+     */
 
-    public void setPopulationCountry(int populationCountry) {
-        if (populationCountry > 0){
-            this.populationCountry = populationCountry;
-        } else {
-            throw new IllegalArgumentException("Ошибка. Население должно быть >0");
-        }
+    //2й ВАРИАНТ условия, через негативную проверку
+    public void setArea(int area) {
+        if (area <= 0)
+            {throw new IllegalArgumentException("Ошибка. Площадь должна быть >0");}
+        this.area = area;
     }
 
-    public void setNameCapital(String nameCapital) {
-        this.nameCapital = nameCapital;
+    public void setPopulation(int population) {
+        if (population <= 0){
+            throw new IllegalArgumentException("Ошибка. Население должно быть >0");
+        }
+        this.population = population;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
     }
 
     public void setAreaCapital(Integer areaCapital) {
@@ -96,30 +122,30 @@ public class Country {
     }
 
     //метод, задающий данные о столице
-    public void setCapitalData(String nameCapital, Integer areaCapital) {
-        this.nameCapital = nameCapital;
+    public void setCapitalData(String capital, Integer areaCapital) {
+        this.capital = capital;
         this.areaCapital = areaCapital;
     }
 
     //метод, сбрасывающий данные о столице;
     public void resetCapitalData() {
-        this.nameCapital = null;
+        this.capital = null;
         this.areaCapital = null;
     }
 
     public void print() {
-        System.out.print("Страна: " + nameCountry);
-        System.out.print(", площадь страны: " + areaCountry + " кв.км.");
-        System.out.print(", население: " + populationCountry + " чел.");
-        if (nameCapital != null) {
-            System.out.print(", столица: " + nameCapital);
+        System.out.print("Страна: " + name);
+        System.out.print(", площадь страны: " + area + " кв.км.");
+        System.out.print(", население: " + population + " чел.");
+        if (capital != null) {
+            System.out.print(", столица: " + capital);
             if (areaCapital != null) {
                 System.out.print(", площадь столицы: " + areaCapital + " кв.км.");
             }
         } else {
             System.out.print("");
         }
-        System.out.println(", плотность населения страны: " + populationDensity + " чел./кв.км.");
+        System.out.println(", плотность населения страны: " + densityPopulation + " чел./кв.км.");
     }
 
     public static void printAll(Country[] countries) {
--- src/main/java/countriespack/Main.java
+++ src/main/java/countriespack/Main.java
@@ -2,16 +2,14 @@ package countriespack;
 
 public class Main {
     public static void main(String[] args) {
-        // Создание массива из 5 стран
-        Country[] countries = new Country[5];
-
-        // Заполнение массива данными
-        countries[0] = new Country("Russia", 17100000, 146700000, "Moscow",12600000);
-        countries[1] = new Country("Finland", 338000, 5500000, "Helsinki",655000);
-        countries[2] = new Country("France", 643800, 67800000, "Paris",2100000);
-        countries[3] = new Country("Andorra", 467, 85400, "Andorra la Vella",22600);
-        countries[4] = new Country("Singapore", 725, 5700000, null,null);
-
+        // Создание массива из стран без ограничения по кол-ву
+        Country[] countries = {
+            new Country("Russia", 17100000, 146700000, "Moscow", 12600000),
+            new Country("Finland", 338000, 5500000, "Helsinki", 655000),
+            new Country("France", 643800, 67800000, "Paris", 2100000),
+            new Country("Andorra", 467, 85400, "Andorra la Vella", 22600),
+            new Country("Singapore", 725, 5700000, null, null),
+        };
         // Вывод информации о каждой стране
         for (Country countr : countries) {
             countr.print();
